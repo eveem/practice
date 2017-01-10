b2eab052f8df744d3f65f74eb6f565d08b9bcb84
eveem <monpriya.tammavong@gmail.com>
2017-01-10 15:50:12 +0700

    Add selectionSort.py

--- selectionSort.py
+++ selectionSort.py
@@ -1,19 +1,19 @@
 list = [99, 2, 14, 505, 1]
 
 def selectionSort(list):
-	start = 0
 	total = len(list)
 	for i in range(0, total):
-		min = list[start]
-		minPose = start
-		for j in range(start, total):
+		min = list[i]
+		minPose = i
+		
+		for j in range(i, total):
 			if list[j] < min:
 				min = list[j]
 				minPose = j
+
 		number = list[minPose]
-		list[minPose] = list[start]
-		list[start] = number
-		start += 1
+		list[minPose] = list[i]
+		list[i] = number
 
 print "Before sort list = " + str(list)
 selectionSort(list)
