25c78f0afdcc4fdff400e4496008d2c7324c3811
eveem <monpriya.tammavong@gmail.com>
2018-02-14 16:38:03 +0700

    project euler 0009

---
+++ 0009_projecteuler.py
@@ -0,0 +1,9 @@
+def result ():
+    for i in range(1, 1000):
+        for j in range(i + 1, 1000):
+            for k in range(j + 1, 1000):
+                if i*i + j*j == k*k and i + j + k == 1000:
+                    print i * j * k
+                    return 0
+
+result()
